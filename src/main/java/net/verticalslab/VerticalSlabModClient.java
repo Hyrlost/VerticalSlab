7afb4576e9cf75993958f90a80c3b97340863c06
Hyrlost <hyrlostcontact@gmail.com>
2022-05-23 22:37:31 -0600

    Removing unnecesary files and code

--- src/main/java/net/verticalslab/VerticalSlabModClient.java
+++ src/main/java/net/verticalslab/VerticalSlabModClient.java
@@ -1,9 +1,6 @@
 package net.verticalslab;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
-import net.verticalslab.classes.VerticalSlabBlock;
-import net.verticalslab.classes.VerticalSlabRenderer;
 
 public class VerticalSlabModClient implements ClientModInitializer {
     @Override
